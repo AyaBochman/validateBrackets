13aab66f264ba689f502a1003e2e3b4e36751f4e
aya <aya.bochman@gmail.com>
2018-11-09 18:34:52 +0200

    code updated

--- index.js
+++ index.js
@@ -9,27 +9,26 @@ return{
 
 const dic = { '{': '}', '[': ']', '(': ')' };
 
-let val = '{[]}';
+
 
 const validateBrackets = () => {
     let stack = [];
+    let result = "valid";
     let val = DOM.input.value;
     for (i = 0; i < val.length; i++) {
         if(val[i] in dic){
             stack.push(dic[val[i]]);
         }else{
-            if(val[i] == stack.slice(-1).pop()){
-                stack.pop();
+            if(val[i] == stack.pop()){
+               continue;
             }else{
-                console.log("Invalid");
-                // return false;
-            }
-            console.log("Valid");
-            // return true;
-           
+                result = "invalid";
+            }   
         }
-      
     }
-  
+    if(stack.length != 0){
+        result = "invalid";
+    }
+    console.log(result);
 }
 
