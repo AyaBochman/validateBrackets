9a4b950f9ac964c3d2aa3a7ac545d51166a97588
aya <aya.bochman@gmail.com>
2018-11-09 17:38:27 +0200

    brackets validation

---
+++ index.js
@@ -0,0 +1,35 @@
+const DOM = function (){
+return{
+    input: document.getElementById("brInput")
+  
+}
+}();
+
+
+
+const dic = { '{': '}', '[': ']', '(': ')' };
+
+let val = '{[]}';
+
+const validateBrackets = () => {
+    let stack = [];
+    let val = DOM.input.value;
+    for (i = 0; i < val.length; i++) {
+        if(val[i] in dic){
+            stack.push(dic[val[i]]);
+        }else{
+            if(val[i] == stack.slice(-1).pop()){
+                stack.pop();
+            }else{
+                console.log("Invalid");
+                // return false;
+            }
+            console.log("Valid");
+            // return true;
+           
+        }
+      
+    }
+  
+}
+
